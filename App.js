4cf468e367369f31631224a583ebd5b2631a1d15
JALASOFT <alcresgu_1989@hotmail.com>
2021-05-03 06:57:00 -0500

    08-Get suggestions movie  from api

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { Text } from 'react-native';
 
 import Home from './src/screens/container/home';
 import Header from './src/sections/components/header';
 import SuggestionsList from './src/videos/container/suggestionsList';
 
+import API from './utils/api'
+
 function App() {
+  useEffect(() => {
+    API.getSuggestions(10);
+  }, [])
   return (
     <Home>
       <Header/>
