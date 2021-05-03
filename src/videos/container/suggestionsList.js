2521986d32053e409d1c5d217bc3ec44b8210268
JALASOFT <alcresgu_1989@hotmail.com>
2021-05-02 21:44:18 -0500

    06-Use properties Flatlist component empty and separator list

--- src/videos/container/suggestionsList.js
+++ src/videos/container/suggestionsList.js
@@ -2,22 +2,35 @@ import React from 'react';
 import { FlatList, Text } from 'react-native';
 
 import Layout from '../components/suggestionListLayout';
+import Empty from '../components/empty';
+import Separator from '../components/verticalSeparator';
 
 function SuggestionsList() {
   const list = [
     {
       key: '01',
-      title: 'Albeiro'
+      title: 'Avengers'
     },
     {
       key: '02',
-      title: 'Crespo'
+      title: 'Batman'
     }
-  ]
+  ];
+
+  function renderEmpty() {
+    return <Empty text="There is not suggestions :(" />
+  };
+
+  function itemSeparator() {
+    return <Separator />
+  }
+
   return (
     <Layout title="Recomendado para ti">
       <FlatList
         data={list}
+        ListEmptyComponent={renderEmpty}
+        ItemSeparatorComponent={itemSeparator}
         renderItem={({ item }) => <Text>{item.title}</Text>}
       />
     </Layout>
