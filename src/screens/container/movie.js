918d4c7527d54f8ff2be2217ab18315329f714c6
galbeiroc <alcresgu_1989@hotmail.com>
2021-10-02 18:11:34 -0500

    29-implement animation

--- src/screens/container/movie.js
+++ src/screens/container/movie.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Animated } from 'react-native';
 import { connect } from 'react-redux';
 import { setSelectedMovie } from '../../redux/actions/actions';
 
@@ -9,18 +10,35 @@ import Player from '../../videos/container/player';
 import Layout from '../components/movieLayout';
 
 function Movie(props) {
+  const [opacity, setOpacity] = useState(new Animated.Value(0));
   function closeVideo() {
     props.dispatch(setSelectedMovie(null))
   };
 
+  useEffect(() => {
+    Animated.timing(
+      opacity,
+      {
+        toValue: 1,
+        duration: 2000,
+        useNativeDriver: true,
+      }
+    ).start();
+  }, [opacity])
+
   return (
-    <Layout>
-      <Header>
-        <Close onPress={closeVideo} />
-      </Header>
-      <Player />
-      <Details {...props.movie} />
-    </Layout>
+    <Animated.View style={{
+      flex: 1,
+      opacity,
+    }}>
+      <Layout>
+        <Header>
+          <Close onPress={closeVideo} />
+        </Header>
+        <Player />
+        <Details {...props.movie} />
+      </Layout>
+    </Animated.View>
   )
 }
 
